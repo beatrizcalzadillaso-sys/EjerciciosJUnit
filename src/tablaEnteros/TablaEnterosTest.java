f9a0297822cd6bc723016d164c74b33b0116c4bf
beatrizcalzadillaso <beatriz.calzadillaso@elorrieta-errekamari.com>
2025-12-15 20:59:46 +0100

    tablaEnteros
    created

---
+++ src/tablaEnteros/TablaEnterosTest.java
@@ -0,0 +1,24 @@
+package tablaEnteros;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TablaEnterosTest {
+
+	@Test
+	public void testSumaTabla() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMayorTabla() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPosicionTabla() {
+		fail("Not yet implemented");
+	}
+
+}
