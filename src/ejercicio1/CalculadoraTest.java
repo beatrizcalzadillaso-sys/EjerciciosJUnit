864f62504759ea5dcf1010fe045dd948e0c0aa5b
beatrizcalzadillaso <beatriz.calzadillaso@elorrieta-errekamari.com>
2025-12-15 20:47:17 +0100

    seven test methods
    working

--- src/ejercicio1/CalculadoraTest.java
+++ src/ejercicio1/CalculadoraTest.java
@@ -23,14 +23,22 @@ public class CalculadoraTest {
 	}
 
 	
-
 	@Test
 	public void testResta() {
 		Calculadora calc= new Calculadora(20,10);
-		String mensaje = "resta2 funciona correctamente";
+		String mensaje = "resta funciona correctamente";
 		int resultado= calc.resta();
 		assertEquals(mensaje, 10,resultado);
 	}
+	
+	@Test
+	public void test2Resta() {
+		Calculadora calc= new Calculadora(10,30);
+		String mensaje = "resta funciona correctamente";
+		int resultado= calc.resta();
+		assertEquals(mensaje, 20,resultado);
+	}
+	
 
 	@Test
 	public void testDivide2() {
@@ -38,6 +46,22 @@ public class CalculadoraTest {
 		String mensaje = "divide2 funciona correctamente";
 		int resultado= calc.divide2();
 		assertEquals(mensaje, 2,resultado);
+	}
+	
+	@Test
+	public void test2Divide2() {
+		Calculadora calc= new Calculadora(20,10);
+		String mensaje = "divide2 funciona correctamente";
+		Object resultado= calc.divide2();
+		assertNotNull(mensaje, resultado);
+	}
+	
+	@Test
+	public void test3Divide2() {
+		Calculadora calc= new Calculadora(20,0);
+		String mensaje = "divide2 funciona correctamente";
+		Object resultado= calc.divide2();
+		assertNull(mensaje, resultado);
 	}
 
 }
