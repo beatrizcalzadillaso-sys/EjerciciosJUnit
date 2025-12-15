455c903e6622a70deeb6363ca6cbb11b4d9c3155
beatrizcalzadillaso <beatriz.calzadillaso@elorrieta-errekamari.com>
2025-12-15 19:42:13 +0100

    Creating first files
    No exercise yet

---
+++ src/ejercicio1/Exercise.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ejercicio1;
+
+/**
+ * 
+ */
+public class Exercise {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
